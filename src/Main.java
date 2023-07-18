85ff1e6bbce487c22f724686d6acdd7d7b842870
tuebeo2001 <nguyenkhactue07@gmail.com>
2023-07-18 23:51:38 +0700

    chore: test rebase

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Effective Java Book");
+        // day la cmt 123
+        // day la cmt 123
+        // day la cmt 123
+        // day la cmt 123
+        // day la cmt 123
+        // day la cmt 123
     }
 }
